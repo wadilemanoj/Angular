c63a7ab62497d36dd52194c33b0f4300056f1fe4
manojwadile <wadilemanoj@outlook.com>
2022-10-20 12:23:29 +0530

    Initial commit

---
+++ contoso.myguard/src/app/core/layout/maincontainer/maincontainer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-maincontainer',
+  templateUrl: './maincontainer.component.html',
+  styleUrls: ['./maincontainer.component.css']
+})
+export class MaincontainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
